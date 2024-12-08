a0e1e279e77d4649a1680f48c5fe3cf015d15970
Martinz-cloud <martinsasanka449@gmail.com>
2024-12-08 18:01:38 +0100

    Add files via upload

---
+++ Kata.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Kata{
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+int number = input.nextInt();
+
+System.out.println("Enter integer: " + number );
+
+
+if (number % 2 == 0){System.out.println("true");
+}else{System.out.print("false");
+}
+
+
+if ( number <= 1){System.out.println("true");
+}else{ for (int figure = 2; figure <= Math.sqrt(number); figure++){
+System.out.print("false"); break;}
+
+
+int[] number = input.nextInt();
+number = int(number1 , number2);
+if (number>1);{System.out.print("positive difference:"(number1 - number2));
+number++;
+}
+
+
+
+
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+}
